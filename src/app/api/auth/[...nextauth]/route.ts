a87f4fd6782de5c6cd1a15287226f78a1d5dd0a8
smi-23 <latack789@naver.com>
2024-07-18 05:24:07 +0900

    feat: session, jwt callback에 리프레시토큰관련 로직 추가, axios 인터셉터에서 하던 로직은 삭제

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { backend_api } from "@/app/utils";
+import jwt from "jsonwebtoken";
+import { signOut } from "next-auth/react";
+import axios from "axios";
 
 const authOptions: NextAuthOptions = {
   providers: [
@@ -22,11 +25,11 @@ const authOptions: NextAuthOptions = {
             user.RefreshToken = response.headers.refreshtoken;
             return user;
           } else {
-            console.error("Error during login:");
+            console.error("로그인 중 오류 발생:");
             return null;
           }
         } catch (error) {
-          console.error("Error during login:", error);
+          console.error("로그인 중 오류 발생:", error);
           return null;
         }
       },
@@ -37,14 +40,52 @@ const authOptions: NextAuthOptions = {
       if (user) {
         token.Authorization = user.Authorization;
         token.RefreshToken = user.RefreshToken;
+        console.log("토큰 콜백 =", token);
+      }
+
+      // 토큰 만료 확인
+      const accessToken = token.Authorization?.replace("Bearer ", "");
+      const decodedToken = jwt.decode(accessToken);
+      if (decodedToken && decodedToken.exp <= Date.now() / 1000) {
+        try {
+          const response = await axios.post(
+            "http://localhost:8080/api/user/refresh-tokens",
+            {},
+            {
+              headers: {
+                Authorization: token.Authorization,
+                RefreshToken: token.RefreshToken,
+              },
+            },
+          );
+          console.log("refresh_tokens response =", response.data);
+          const newAccessToken = response.headers["authorization"];
+          const newRefreshToken = response.headers["refreshtoken"];
+
+          token.Authorization = newAccessToken;
+          token.RefreshToken = newRefreshToken;
+          return token;
+        } catch (error) {
+          console.error("토큰 갱신 실패:", error);
+          await signOut();
+          // 토큰 갱신 실패 시, 유효하지 않은 토큰을 반환하지 않도록 처리
+          return {
+            ...token,
+            Authorization: null,
+            RefreshToken: null,
+          };
+        }
       }
       return token;
     },
 
     async session({ session, token }) {
-      // 세션에 저장할 필드 선택
-      session.user.Authorization = token.Authorization;
-      session.user.RefreshToken = token.RefreshToken;
+      if (session) {
+        // session.user = session.user || {}; // session.user가 undefined인 경우 빈 객체로 초기화
+        session.user.Authorization = token.Authorization;
+        session.user.RefreshToken = token.RefreshToken;
+        console.log("세션 콜백 =", session);
+      }
       return session;
     },
   },
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,99 +1,36 @@
 import axios from "axios";
-import { getSession, signIn, signOut } from "next-auth/react";
-import jwt from "jsonwebtoken";
+import { getSession } from "next-auth/react";
 
-const SPRING_URL =
+const BACKEND_SPRING_URL =
   process.env.NODE_ENV === "production" ? "배포 url" : "http://localhost:8080";
 
 export const backend_api = () => {
   const defaultOptions = {
-    baseURL: SPRING_URL,
+    baseURL: BACKEND_SPRING_URL,
   };
 
   const instance = axios.create(defaultOptions);
 
-  instance.interceptors.request.use(async (request) => {
-    const session = await getSession();
-    if (session) {
-      request.headers.Authorization = session?.user?.Authorization;
-      request.headers.RefreshToken = session?.user?.RefreshToken;
-    }
-    return request;
-  });
+  instance.interceptors.request.use(
+    (config) => {
+      const session = getSession();
+      if (session) {
+        config.headers.Authorization = session?.user?.Authorization;
+        config.headers.RefreshToken = session?.user?.RefreshToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    },
+  );
 
   instance.interceptors.response.use(
     (response) => {
       console.log(response.data, response.status);
       return response;
     },
-    async (error) => {
-      const originalRequest = error.config;
-
-      if (error.response.status === 401) {
-        const session = await getSession();
-
-        if (session) {
-          const token = session.user?.Authorization?.replace("Bearer ", "");
-
-          if (token) {
-            const decodedToken = jwt.decode(token);
-            const currentTime = Date.now() / 1000;
-
-            if (decodedToken.exp < currentTime) {
-              try {
-                const response = await axios.post(
-                  `${SPRING_URL}/api/user/refresh-tokens`,
-                  {},
-                  {
-                    headers: {
-                      RefreshToken: session.user?.RefreshToken,
-                      Authorization: session.user?.Authorization,
-                    },
-                  },
-                );
-                // 새로 발급받은 토큰 정보 업데이트
-                const newAccessToken = response.headers["authorization"];
-                const newRefreshToken = response.headers["refreshtoken"];
-                console.log("바뀌기 전 세션 전체", session.user);
-                console.log("바뀌기 전", session.user.Authorization);
-                console.log("바뀌기 전", session.user.RefreshToken);
-
-                // session.user.Authorization = newAccessToken;
-                // session.user.RefreshToken = newRefreshToken;
-
-                sessionStorage.setItem("Authorization", newAccessToken);
-                sessionStorage.setItem("RefreshToken", newRefreshToken);
-
-                console.log("바뀐 후 세션 전체", session.user);
-                console.log("바뀐거", session.user.Authorization);
-                console.log("바뀐거", session.user.RefreshToken);
-
-                originalRequest.headers.Authorization = newAccessToken;
-                originalRequest.headers.RefreshToken = newRefreshToken;
-
-                return axios(originalRequest);
-              } catch (err) {
-                // 새 토큰 발급에 실패한 경우
-                // signOut();
-                return Promise.reject(err);
-              }
-            } else {
-              console.log("토큰의 유효기간이 남아 있는데 인증 오류인 경우");
-
-              // 토큰의 유효 기간이 남아 있는데 인증 오류인 경우
-              // signOut();
-            }
-          } else {
-            console.log("토큰이 없는 경우", token);
-            // 토큰이 없는 경우
-            // signOut();
-          }
-        } else {
-          console.log("seesion 없는 경우", session);
-          // 세션이 없는 경우
-          // signOut();
-        }
-      }
+    (error) => {
       console.log(`Error log`, error.response.data, error.response.status);
       return Promise.reject(error);
     },
